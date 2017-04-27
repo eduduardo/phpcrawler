1213bdb572b2c1aa60c3cef84c6b18525df8ec19
Eduardo Augusto Ramos <eduduardo@users.noreply.github.com>
2017-04-27 10:22:12 -0300

    Adding fragment to url parts

--- libs/Utils/PHPCrawlerUtils.class.php
+++ libs/Utils/PHPCrawlerUtils.class.php
@@ -29,22 +29,23 @@ class PHPCrawlerUtils
     // Protokoll der URL hinzufügen (da ansonsten parse_url nicht klarkommt)
     if (!preg_match("#^[a-z0-9-]+://# i", $url))
       $url = "http://" . $url;
-    
+
     $parts = @parse_url($url);
-    
+
     if (!isset($parts)) return null;
-    
+
     $protocol = $parts["scheme"]."://";
     $host = (isset($parts["host"]) ? $parts["host"] : "");
     $path = (isset($parts["path"]) ? $parts["path"] : "");
     $query = (isset($parts["query"]) ? "?".$parts["query"] : "");
+    $fragment = (isset($parts["fragment"]) ? "/#".$parts["fragment"] : "");
     $auth_username = (isset($parts["user"]) ? $parts["user"] : "");
     $auth_password = (isset($parts["pass"]) ? $parts["pass"] : "");
     $port = (isset($parts["port"]) ? $parts["port"] : "");
-    
+
     // Host is case-insensitive
     $host = strtolower($host);
-    
+
     // File
     preg_match("#^(.*/)([^/]*)$#", $path, $match); // Alles ab dem letzten "/"
     if (isset($match[0]))
@@ -56,7 +57,7 @@ class PHPCrawlerUtils
     {
       $file = "";
     }
-      
+
     // The domainname from the host
     // Host: www.foo.com -> Domain: foo.com
     $parts = @explode(".", $host);
@@ -73,12 +74,12 @@ class PHPCrawlerUtils
       $pos = strpos($host, ".");
       $domain = substr($host, $pos+1);
     }
-    
+
     // DEFAULT VALUES für protocol, path, port etc. (wenn noch nicht gesetzt)
-      
+
     // Wenn Protokoll leer -> Protokoll ist "http://"
     if ($protocol == "") $protocol="http://";
-    
+
     // Wenn Port leer -> Port setzen auf 80 or 443
     // (abhängig vom Protokoll)
     if ($port == "")
@@ -86,25 +87,26 @@ class PHPCrawlerUtils
       if (strtolower($protocol) == "http://") $port=80;
       if (strtolower($protocol) == "https://") $port=443;
     }
-    
+
     // Wenn Pfad leet -> Pfad ist "/"
     if ($path=="") $path = "/";
-    
+
     // Rückgabe-Array
     $url_parts["protocol"] = $protocol;
     $url_parts["host"] = $host;
     $url_parts["path"] = $path;
     $url_parts["file"] = $file;
     $url_parts["query"] = $query;
+    $url_parts["fragment"] = $fragment;
     $url_parts["domain"] = $domain;
     $url_parts["port"] = $port;
-    
+
     $url_parts["auth_username"] = $auth_username;
     $url_parts["auth_password"] = $auth_password;
-    
+
     return $url_parts;
   }
-  
+
   /**
    * Builds an URL from it's single parts.
    *
@@ -121,7 +123,7 @@ class PHPCrawlerUtils
    * @param bool $normalize   If TRUE, the URL will be returned normalized.
    *                          (I.e. http://www.foo.com/path/ insetad of http://www.foo.com:80/path/)
    * @return string The URL
-   *                         
+   *
    */
   public static function buildURLFromParts($url_parts, $normalize = false)
   {
@@ -130,25 +132,26 @@ class PHPCrawlerUtils
     {
       throw new Exception("Cannot generate URL, host not specified!");
     }
-    
+
     if (!isset($url_parts["protocol"]) || $url_parts["protocol"] == "") $url_parts["protocol"] = "http://";
     if (!isset($url_parts["port"])) $url_parts["port"]= 80;
     if (!isset($url_parts["path"])) $url_parts["path"] = "";
     if (!isset($url_parts["file"])) $url_parts["file"] = "";
     if (!isset($url_parts["query"])) $url_parts["query"]= "";
+    if (!isset($url_parts["fragment"])) $url_parts["fragment"]= "";
     if (!isset($url_parts["auth_username"])) $url_parts["auth_username"]= "";
     if (!isset($url_parts["auth_password"])) $url_parts["auth_password"]= "";
-    
+
     // Autentication-part
     $auth_part = "";
     if ($url_parts["auth_username"] != "" && $url_parts["auth_password"] != "")
     {
       $auth_part = $url_parts["auth_username"].":".$url_parts["auth_password"]."@";
     }
-    
+
     // Port-part
     $port_part = ":" . $url_parts["port"];
-    
+
     // Normalize
     if ($normalize == true)
     {
@@ -157,23 +160,23 @@ class PHPCrawlerUtils
       {
         $port_part = "";
       }
-      
+
       // Don't add port to links other than "http://" or "https://"
       if ($url_parts["protocol"] != "http://" && $url_parts["protocol"] != "https://")
       {
         $port_part = "";
       }
     }
-    
+
     // If path is just a "/" -> remove it ("www.site.com/" -> "www.site.com")
     if ($url_parts["path"] == "/" && $url_parts["file"] == "" && $url_parts["query"] == "") $url_parts["path"] = "";
-    
+
     // Put together the url
-    $url = $url_parts["protocol"] . $auth_part . $url_parts["host"]. $port_part . $url_parts["path"] . $url_parts["file"] . $url_parts["query"];
-    
+    $url = $url_parts["protocol"] . $auth_part . $url_parts["host"]. $port_part . $url_parts["path"] . $url_parts["file"] . $url_parts["query"] . $url_parts["fragment"];
+
     return $url;
   }
-  
+
   /**
    * Normalizes an URL
    *
@@ -185,13 +188,13 @@ class PHPCrawlerUtils
   public static function normalizeURL($url)
   {
     $url_parts = self::splitURL($url);
-    
+
     if ($url_parts == null) return null;
-    
+
     $url_normalized = self::buildURLFromParts($url_parts, true);
     return $url_normalized;
   }
-  
+
   /**
    * Checks whether a given RegEx-pattern is valid or not.
    *
@@ -203,7 +206,7 @@ class PHPCrawlerUtils
     if (is_integer($check) == false) return false;
     else return true;
   }
-  
+
   /**
    * Gets the HTTP-statuscode from a given response-header.
    *
@@ -213,15 +216,15 @@ class PHPCrawlerUtils
   public static function getHTTPStatusCode($header)
   {
     $first_line = strtok($header, "\n");
-    
+
     preg_match("# [0-9]{3}#", $first_line, $match);
-    
+
     if (isset($match[0]))
       return (int)trim($match[0]);
     else
       return null;
   }
-  
+
   /**
    * Reconstructs a full qualified and normalized URL from a given link relating to the URL the link was found in.
    *
@@ -232,84 +235,84 @@ class PHPCrawlerUtils
    *                or NULL if the link couldn't be rebuild correctly.
    */
   public static function buildURLFromLink($link, PHPCrawlerUrlPartsDescriptor $BaseUrl)
-  { 
+  {
     $url_parts = $BaseUrl->toArray();
-    
+
     // Dedoce HTML-entities
     $link = PHPCrawlerEncodingUtils::decodeHtmlEntities($link);
-    
+
     // Remove anchor ("#..."), but ONLY at the end, not if # is at the beginning !
     $link = preg_replace("/^(.{1,})#.{0,}$/", "\\1", $link);
 
     // Cases
-    
+
     // Strange link like "//foo.htm" -> make it to "http://foo.html"
     if (substr($link, 0, 2) == "//")
     {
       $link = "http:".$link;
     }
-   
+
     // 1. relative link starts with "/" --> doc_root
-    // "/index.html" -> "http://www.foo.com/index.html"    
+    // "/index.html" -> "http://www.foo.com/index.html"
     elseif (substr($link,0,1)=="/")
     {
       $link = $url_parts["protocol"].$url_parts["host"].":".$url_parts["port"].$link;
     }
-     
+
      // 2. "./foo.htm" -> "foo.htm"
      elseif (substr($link,0,2)=="./")
      {
        $link=$url_parts["protocol"].$url_parts["host"].":".$url_parts["port"].$url_parts["path"].substr($link, 2);
      }
-     
+
      // 3. Link is an absolute Link with a given protocol and host (f.e. "http://..." or "android-app://...)
      // DO NOTHING
      elseif (preg_match("#^[a-z0-9-]{1,}(:\/\/)# i", $link))
      {
        $link = $link;
      }
-     
+
      // 4. Link is stuff like "javascript: ..." or something
      elseif (preg_match("/^[a-zA-Z]{0,}:[^\/]{0,1}/", $link))
      {
        $link = "";
      }
-    
+
      // 5. "../../foo.html" -> remove the last path from our actual path
      // and remove "../" from link at the same time until there are
      // no more "../" at the beginning of the link
      elseif (substr($link, 0, 3)=="../")
      {
        $new_path = $url_parts["path"];
-       
+
        while (substr($link, 0, 3) == "../")
        {
          $new_path = preg_replace('/\/[^\/]{0,}\/$/',"/", $new_path);
          $link  = substr($link, 3);
        }
-       
+
        $link = $url_parts["protocol"].$url_parts["host"].":".$url_parts["port"].$new_path.$link;
      }
-     
+
      // 6. link starts with #
      // -> leads to the same site as we are on, trash
      elseif (substr($link,0,1) == "#")
      {
        $link="";
      }
-     
+
      // 7. link starts with "?"
      elseif (substr($link,0,1)=="?")
      {
        $link = $url_parts["protocol"].$url_parts["host"].":".$url_parts["port"].$url_parts["path"].$url_parts["file"].$link;
      }
-    
+
      // 7. thats it, else the abs_path is simply PATH.LINK ...
      else
-     { 
+     {
        $link = $url_parts["protocol"].$url_parts["host"].":".$url_parts["port"].$url_parts["path"].$link;
      }
-     
+
      if ($link == "") return null;
 
      // Now, at least, replace all HTMLENTITIES with normal text.
@@ -320,13 +323,13 @@ class PHPCrawlerUtils
      // Replace linebreaks in the link with "" (happens if a link in the sourcecode
      // linebreaks)
      $link = str_replace(array("\n", "\r"), "", $link);
-     
+
      // "Normalize" URL
      $link = self::normalizeUrl($link);
-      
+
      return $link;
   }
-  
+
   /**
    * Returns the base-URL specified in a meta-tag in the given HTML-source
    *
@@ -335,7 +338,7 @@ class PHPCrawlerUtils
   public static function getBaseUrlFromMetaTag(&$html_source)
   {
     preg_match("#<{1}[ ]{0,}((?i)base){1}[ ]{1,}((?i)href|src)[ ]{0,}=[ ]{0,}(\"|'){0,1}([^\"'><\n ]{0,})(\"|'|>|<|\n| )# i", $html_source, $match);
-    
+
     if (isset($match[4]))
     {
       $match[4] = trim($match[4]);
@@ -343,7 +346,7 @@ class PHPCrawlerUtils
     }
     else return null;
   }
-  
+
   /**
    * Returns the redirect-URL from the given HTML-header
    *
@@ -353,7 +356,7 @@ class PHPCrawlerUtils
   {
     // Get redirect-link from header
     preg_match("/((?i)location:|content-location:)(.{0,})[\n]/", $header, $match);
-    
+
     if (isset($match[2]))
     {
       $redirect = trim($match[2]);
@@ -361,7 +364,7 @@ class PHPCrawlerUtils
     }
     else return null;
   }
-  
+
   /**
    * Checks whether a given string matches with one of the given regular-expressions.
    *
@@ -373,7 +376,7 @@ class PHPCrawlerUtils
   public static function checkStringAgainstRegexArray(&$string, $regex_array)
   {
     if (count($regex_array) == 0) return true;
-    
+
     $cnt = count($regex_array);
     for ($x=0; $x<$cnt; $x++)
     {
@@ -382,10 +385,10 @@ class PHPCrawlerUtils
         return true;
       }
     }
-    
+
     return false;
   }
-  
+
   /**
    * Gets the value of an header-directive from the given HTTP-header.
    *
@@ -401,15 +404,15 @@ class PHPCrawlerUtils
   public static function getHeaderValue($header, $directive)
   {
     preg_match("#[\r\n]".$directive.":(.*)[\r\n\;]# Ui", $header, $match);
-    
+
     if (isset($match[1]) && trim($match[1]) != "")
     {
       return trim($match[1]);
     }
-    
+
     else return null;
   }
-  
+
   /**
    * Returns all cookies from the give response-header.
    *
@@ -420,9 +423,9 @@ class PHPCrawlerUtils
   public static function getCookiesFromHeader($header, $source_url)
   {
     $cookies = array();
-    
+
     $hits = preg_match_all("#[\r\n]set-cookie:(.*)[\r\n]# Ui", $header, $matches);
-    
+
     if ($hits && $hits != 0)
     {
       for ($x=0; $x<count($matches[1]); $x++)
@@ -430,10 +433,10 @@ class PHPCrawlerUtils
         $cookies[] = PHPCrawlerCookieDescriptor::getFromHeaderLine($matches[1][$x], $source_url);
       }
     }
-    
+
     return $cookies;
   }
-  
+
   /**
    * Returns the normalized root-URL of the given URL
    *
@@ -444,10 +447,10 @@ class PHPCrawlerUtils
   {
     $url_parts = self::splitURL($url);
     $root_url = $url_parts["protocol"].$url_parts["host"].":".$url_parts["port"];
-    
+
     return self::normalizeURL($root_url);
   }
-  
+
   /**
    * Deletes a directory recursivly
    */
@@ -467,11 +470,11 @@ class PHPCrawlerUtils
         }
       }
       reset($objects);
-      
+
       rmdir($dir);
     }
-  } 
-  
+  }
+
   /**
    * Serializes data (objects, arrays etc.) and writes it to the given file.
    */
@@ -480,7 +483,7 @@ class PHPCrawlerUtils
     $serialized_data = serialize($data);
     file_put_contents($target_file, $serialized_data);
   }
-  
+
   /**
    * Returns deserialized data that is stored in a file.
    *
@@ -497,17 +500,17 @@ class PHPCrawlerUtils
     }
     else return null;
   }
-  
+
   /**
    * Sorts a twodimensiolnal array.
    */
   public static function sort2dArray(&$array, $sort_args)
   {
     $args = func_get_args();
-    
+
     // Für jedes zu sortierende Feld ein eigenes Array bilden
     @reset($array);
-    while (list($field) = @each($array)) 
+    while (list($field) = @each($array))
     {
       for ($x=1; $x<count($args); $x++)
       {
@@ -515,7 +518,7 @@ class PHPCrawlerUtils
         if (is_string($args[$x]))
         {
           $value = $array[$field][$args[$x]];
-          
+
           ${$args[$x]}[] = $value;
         }
       }
@@ -535,16 +538,16 @@ class PHPCrawlerUtils
         $params[] = &$args[$x];
       }
     }
-    
+
     // Der letzte Parameter ist immer das zu sortierende Array (Referenz!)
     $params[] = &$array;
 
     // Array sortieren
     call_user_func_array("array_multisort", $params);
-    
+
     @reset($array);
   }
-  
+
   /**
    * Determinates the systems temporary-directory.
    *
@@ -555,7 +558,7 @@ class PHPCrawlerUtils
     $dir = sys_get_temp_dir()."/";
     return $dir;
   }
-  
+
   /**
    * Gets all meta-tag atteributes from the given HTML-source.
    *
@@ -566,36 +569,36 @@ class PHPCrawlerUtils
    *
    */
   public static function getMetaTagAttributes(&$html_source)
-  {                
+  {
     preg_match_all("#<\s*meta\s+".
                    "name\s*=\s*(?|\"([^\"]+)\"|'([^']+)'|([^\s><'\"]+))\s+".
                    "content\s*=\s*(?|\"([^\"]+)\"|'([^']+)'|([^\s><'\"]+))".
                    ".*># Uis", $html_source, $matches);
-    
-    $tags = array();            
+
+    $tags = array();
     for ($x=0; $x<count($matches[0]); $x++)
     {
       $meta_name = strtolower(trim($matches[1][$x]));
       $meta_value = strtolower(trim($matches[2][$x]));
-      
+
       $tags[$meta_name] = $meta_value;
     }
-    
+
     return $tags;
   }
-  
+
   /**
    * Checks whether the given string is a valid, urlencoded URL (by RFC)
-   * 
+   *
    * @param string $string The string
    * @return bool TRUE if the string is a valid url-string.
    */
   public static function isValidUrlString($string)
-  { 
+  {
     if (preg_match("#^[a-z0-9/.&=?%-_.!~*'()]+$# i", $string)) return true;
     else return false;
   }
-  
+
   /**
    * Gets the content from the given file or URL
    *
@@ -607,48 +610,48 @@ class PHPCrawlerUtils
   public static function getURIContent($uri, $request_user_agent_string = null, $throw_exception = false)
   {
     $UriParts = PHPCrawlerUrlPartsDescriptor::fromURL($uri);
-    
+
     $error_str = "";
-    
+
     // If protocol is "file"
     if ($UriParts->protocol == "file://")
     {
       $file = preg_replace("#^file://#", "", $uri);
-      
+
       if (file_exists($file) && is_readable($file))
         return file_get_contents($file);
       else
         $error_str = "Error reading from file '".$file."'";
     }
-    
+
     // If protocol is "http" or "https"
     elseif ($UriParts->protocol == "http://" || $UriParts->protocol == "https://")
     {
       $uri = self::normalizeURL($uri);
       $Request = new PHPCrawlerHTTPRequest();
       $Request->setUrl(new PHPCrawlerURLDescriptor($uri));
-      
+
       if ($request_user_agent_string !== null)
         $Request->userAgentString = $request_user_agent_string;
-      
+
       $DocInfo = $Request->sendRequest();
-      
+
       if ($DocInfo->received == true)
         return $DocInfo->source;
       else
         $error_str = "Error reading from URL '".$uri."'";
     }
-    
+
     // if protocol is not supported
     else
     {
       $error_str = "Unsupported protocol-type '".$UriParts->protocol."'";
     }
-    
+
     // Throw exception?
     if ($throw_exception == true)
       throw new Exception($error_str);
-    
+
     return null;
   }
 }
